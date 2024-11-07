51b758b777a37889294fc6381b3b4e1841bc00eb
zaydam1000000 <86502397+zayndamji@users.noreply.github.com>
2024-11-07 09:04:17 -0800

    fixed serving of index.html

--- app.js
+++ app.js
@@ -2,7 +2,9 @@ const express = require('express');
 const app = express();
 
 app.get('/', (req, res) => {
-  res.sendFile('index.html');
+  res.sendFile('index.html', { root: __dirname });
 });
 
-app.listen(2244);
+app.listen(2244, () => {
+  console.log('Running on http://localhost:2244/');
+});

9105dd37df405089c8ee2590e6751ec71a62b25c
zaydam1000000 <86502397+zayndamji@users.noreply.github.com>
2024-11-07 09:32:43 -0800

    updates unit at init of page

--- script.js
+++ script.js
@@ -1,5 +1,14 @@
-document.getElementById('unit-select').addEventListener('change', async () => {
+let currentlyUpdatingUnit = false;
+async function updateUnit() {
+  if (currentlyUpdatingUnit) return;
+  currentlyUpdatingUnit = true;
+
   const res = await fetch('/unit/' + document.getElementById('unit-select').value);
   const json = await res.json();
   console.log(json);
-});
+
+  currentlyUpdatingUnit = false;
+}
+
+document.getElementById('unit-select').addEventListener('change', updateUnit);
+updateUnit();

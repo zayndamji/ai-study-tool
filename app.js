1492855d2b09ab4a69767efed5c26874d3118b6f
zaydam1000000 <86502397+zayndamji@users.noreply.github.com>
2024-11-07 09:16:50 -0800

    added basic styles.css and script.js

--- app.js
+++ app.js
@@ -5,6 +5,14 @@ app.get('/', (req, res) => {
   res.sendFile('index.html', { root: __dirname });
 });
 
+app.get('/styles.css', (req, res) => {
+  res.sendFile('styles.css', { root: __dirname });
+});
+
+app.get('/script.js', (req, res) => {
+  res.sendFile('script.js', { root: __dirname });
+});
+
 app.listen(2244, () => {
   console.log('Running on http://localhost:2244/');
 });
